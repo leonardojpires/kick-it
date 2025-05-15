f6fda2c0aef4a4e0a1e282d082002a00ecab5ea4
gnzz999 <goncalo.amferreira@gmail.com>
2025-05-15 11:38:16 +0100

    Base do Projeto

--- kick-it/resources/views/index.blade.php
+++ kick-it/resources/views/index.blade.php
@@ -6,6 +6,8 @@
         <div class="col-md-12">
             <h1>Welcome to Kick It!</h1>
             <p>Your one-stop solution for all your needs.</p>
+
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil architecto sunt recusandae odio doloribus nesciunt eius soluta consequatur laborum non ipsum perferendis ad pariatur voluptas quia nemo debitis, repellendus iure! </p>
         </div>
     </div>
 </div>
