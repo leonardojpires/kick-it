fbbfaae7c9b299c0fa24e1b984c5dbc7b4f459f2
gnzz999 <goncalo.amferreira@gmail.com>
2025-05-15 10:09:07 +0100

    Base Funcional

---
+++ kick-it/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.fe_master')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Welcome to Kick It!</h1>
+            <p>Your one-stop solution for all your needs.</p>
+        </div>
+    </div>
+</div>
+@endsection
